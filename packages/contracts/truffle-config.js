76936ea2cb391ec973829ba5aabef5d38e582b46
Patrick <blockchainbuddha@gmail.com>
2020-10-28 02:09:30 -0400

    import from OZ

--- packages/contracts/truffle-config.js
+++ packages/contracts/truffle-config.js
@@ -28,7 +28,6 @@ const HDWalletProvider = require("@truffle/hdwallet-provider");
 require("dotenv").config();
 const GAS_LIMIT = 8e6;
 
-
 module.exports = {
   /**
    * Networks define how you connect to your ethereum client and let you set the
@@ -79,18 +78,18 @@ module.exports = {
     // }
 
     goerli: {
-        provider: () => new HDWalletProvider(
-            process.env.GOERLI_MNEMONIC,
-            process.env.GOERLI_PROVIDER_URL,
+      provider: () =>
+        new HDWalletProvider(
+          process.env.GOERLI_MNEMONIC,
+          process.env.GOERLI_PROVIDER_URL
         ),
-        network_id: 5, // Goerli's id
-        gas: GAS_LIMIT,
-        gasPrice: 10e9, // 10 GWEI
-        //confirmations: 6, // # of confs to wait between deployments. (default: 0)
-        timeoutBlocks: 50, // # of blocks before a deployment times out  (minimum/default: 50)
-        skipDryRun: false // Skip dry run before migrations? (default: false for public nets )
+      network_id: 5, // Goerli's id
+      gas: GAS_LIMIT,
+      gasPrice: 10e9, // 10 GWEI
+      //confirmations: 6, // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 50, // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: false, // Skip dry run before migrations? (default: false for public nets )
     },
-
   },
 
   // Set default mocha options here, use special reporters etc.
@@ -101,7 +100,7 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      version: "0.7.1",    // Fetch exact version from solc-bin (default: truffle's version)
+      version: "0.7.0", // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       // settings: {          // See the solidity docs for advice about optimization and evmVersion
       //  optimizer: {
