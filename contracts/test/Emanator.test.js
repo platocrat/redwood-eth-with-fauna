3596034efad9ccbc9352adfeed258397bacebeba
Victor Rortvedt <victorrortvedt@gmail.com>
2020-10-31 14:53:11 -0400

    update multi bid test

--- contracts/test/Emanator.test.js
+++ contracts/test/Emanator.test.js
@@ -142,7 +142,9 @@ contract('Emanator', (accounts) => {
     await web3tx(app.bid, `Account ${bob} bids 10`)(toWad(10), { from: bob })
     appRealtimeBalance = await printRealtimeBalance("App", app.address);
     await printRealtimeBalance("Bob", bob);
-    await web3tx(app.checkTimeRemaining, `Time remaining: ${timeLeft}`)({ from: bob })
+    let timeLeft
+    timeLeft = await web3tx(app.checkTimeRemaining)({ from: bob })
+    console.log(timeLeft)
     await web3tx(app.bid, `Account ${carol} bids 20`)(toWad(20), { from: carol })
     appRealtimeBalance = await printRealtimeBalance("App", app.address);
     await printRealtimeBalance("Carol", carol);
