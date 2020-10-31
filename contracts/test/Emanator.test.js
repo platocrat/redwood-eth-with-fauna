69bb8d947bea711b2427b919b0095a7a98ec1b47
Victor Rortvedt <victorrortvedt@gmail.com>
2020-10-31 15:09:27 -0400

    update tests

--- contracts/test/Emanator.test.js
+++ contracts/test/Emanator.test.js
@@ -142,6 +142,7 @@ contract('Emanator', (accounts) => {
     await web3tx(app.bid, `Account ${bob} bids 10`)(toWad(10), { from: bob })
     appRealtimeBalance = await printRealtimeBalance("App", app.address);
     await printRealtimeBalance("Bob", bob);
+    console.log(block.timestamp)
     let timeLeft = await app.checkTimeRemaining()
     console.log(timeLeft)
     await web3tx(app.bid, `Account ${carol} bids 20`)(toWad(20), { from: carol })
