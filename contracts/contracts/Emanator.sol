0e9e7645a6808084cdfd568f233cdbe39162fa75
Patrick <blockchainbuddha@gmail.com>
2020-11-02 14:03:57 -0500

    revert back to uint

--- contracts/contracts/Emanator.sol
+++ contracts/contracts/Emanator.sol
@@ -1,23 +1,74 @@
 pragma solidity ^0.7.0;
 
 contract DSMath {
-    function add(uint128 x, uint128 y) internal pure returns (uint128 z) {
+    function add(uint x, uint y) internal pure returns (uint z) {
         require((z = x + y) >= x, "ds-math-add-overflow");
     }
-
-    function mul(uint128 x, uint128 y) internal pure returns (uint128 z) {
+    function sub(uint x, uint y) internal pure returns (uint z) {
+        require((z = x - y) <= x, "ds-math-sub-underflow");
+    }
+    function mul(uint x, uint y) internal pure returns (uint z) {
         require(y == 0 || (z = x * y) / y == x, "ds-math-mul-overflow");
     }
 
-    uint128 constant WAD = 10 ** 18;
+    function min(uint x, uint y) internal pure returns (uint z) {
+        return x <= y ? x : y;
+    }
+    function max(uint x, uint y) internal pure returns (uint z) {
+        return x >= y ? x : y;
+    }
+    function imin(int x, int y) internal pure returns (int z) {
+        return x <= y ? x : y;
+    }
+    function imax(int x, int y) internal pure returns (int z) {
+        return x >= y ? x : y;
+    }
+
+    uint constant WAD = 10 ** 18;
+    uint constant RAY = 10 ** 27;
 
     //rounds to zero if x*y < WAD / 2
-    function wmul(uint128 x, uint128 y) internal pure returns (uint128 z) {
+    function wmul(uint x, uint y) internal pure returns (uint z) {
         z = add(mul(x, y), WAD / 2) / WAD;
     }
     //rounds to zero if x*y < WAD / 2
-    function wdiv(uint128 x, uint128 y) internal pure returns (uint128 z) {
+    function rmul(uint x, uint y) internal pure returns (uint z) {
+        z = add(mul(x, y), RAY / 2) / RAY;
+    }
+    //rounds to zero if x*y < WAD / 2
+    function wdiv(uint x, uint y) internal pure returns (uint z) {
         z = add(mul(x, WAD), y / 2) / y;
+    }
+    //rounds to zero if x*y < RAY / 2
+    function rdiv(uint x, uint y) internal pure returns (uint z) {
+        z = add(mul(x, RAY), y / 2) / y;
+    }
+
+    // This famous algorithm is called "exponentiation by squaring"
+    // and calculates x^n with x as fixed-point and n as regular unsigned.
+    //
+    // It's O(log n), instead of O(n) for naive repeated multiplication.
+    //
+    // These facts are why it works:
+    //
+    //  If n is even, then x^n = (x^2)^(n/2).
+    //  If n is odd,  then x^n = x * x^(n-1),
+    //   and applying the equation for even x gives
+    //    x^n = x * (x^2)^((n-1) / 2).
+    //
+    //  Also, EVM division is flooring and
+    //    floor[(n-1) / 2] = floor[n / 2].
+    //
+    function rpow(uint x, uint n) internal pure returns (uint z) {
+        z = n % 2 != 0 ? x : RAY;
+
+        for (n /= 2; n != 0; n /= 2) {
+            x = rmul(x, x);
+
+            if (n % 2 != 0) {
+                z = rmul(z, x);
+            }
+        }
     }
 }
 
@@ -36,7 +87,7 @@ contract Emanator is ERC721, IERC721Receiver, DSMath {
   uint32 public winLength;
   uint32 public currentGeneration;
 
-  uint128[] public ownerRevShares = [1*10**18];
+  uint[] public ownerRevShares = [1*10**18];
 
   address payable public creator;
   address[] public revShareRecipients;
@@ -150,21 +201,21 @@ contract Emanator is ERC721, IERC721Receiver, DSMath {
       ERC721._safeMint(_auction.highBidder, currentGeneration);
 
       // Distribute tokens
-      uint128 distributeAmount = wmul(tokenX.balanceOf(address(this)), wdiv(7, 10));
+      uint distributeAmount = rmul(tokenX.balanceOf(address(this)), rdiv(7, 10));
       host.callAgreement(
           ida,
           abi.encodeWithSelector(
               ida.distribute.selector,
               tokenX,
               INDEX_ID,
-              uint128(distributeAmount),
+              distributeAmount,
               new bytes(0)
           )
       );
       tokenX.transfer(creator, tokenX.balanceOf(address(this)));
 
       // Update the shares
-      uint128 newShares = wmul(ownerRevShares[ownerRevShares.length], wdiv(9, 10));
+      uint newShares = rmul(ownerRevShares[ownerRevShares.length], rdiv(9, 10));
       ownerRevShares.push(newShares);
       revShareRecipients.push(_auction.highBidder);
       host.callAgreement(
@@ -174,7 +225,7 @@ contract Emanator is ERC721, IERC721Receiver, DSMath {
           tokenX,
           INDEX_ID,
           _auction.highBidder,
-          uint128(newShares),
+          newShares,
           new bytes(0)
         )
       );
