6b50587fcfdf7cbcc20340847357e11edcb9fb90
Patrick <blockchainbuddha@gmail.com>
2020-11-04 17:40:50 -0500

    remove untracked files

--- .redwood/index.d.ts
+++
@@ -1,4 +0,0 @@
-// 2020-11-03T20:53:51.821Z
-/// <reference path="./types/import-dir-schemas.d.ts" />
-/// <reference path="./types/import-dir-services.d.ts" />
-/// <reference path="./types/routes.d.ts" />
